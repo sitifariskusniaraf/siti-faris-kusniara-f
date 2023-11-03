0373c97e9ca61d7946cfa7e0b3fd9b3eb0625e0a
sitifariskusniaraf <145628937+sitifariskusniaraf@users.noreply.github.com>
2023-11-03 08:18:44 +0700

    Add files via upload

---
+++ PERTEMUAN02/Player.java
@@ -0,0 +1,20 @@
+package PERTEMUAN02;
+
+public class Player {
+    String name ;
+    int speed;
+    int healthpoint;
+    
+   void run(){
+        System.out.println(name + " is running... ");
+        System.out.println("Speed" + speed);
+    }
+    
+    
+    boolean IsDead(){
+        if(healthpoint <= 0) return true;
+    return false;
+    }
+    public static void main(String[] args){
+}
+}
---
+++ PERTEMUAN02/hitungLuasLingkaran.java
@@ -0,0 +1,9 @@
+package PERTEMUAN02;
+public class hitungLuasLingkaran {
+    public static void main(String[] args) {
+        luasLingkaran L = new luasLingkaran();
+        System.out.println("Luas Lingkaran : " + L.hitungLuasL());
+        
+    }
+    
+}
