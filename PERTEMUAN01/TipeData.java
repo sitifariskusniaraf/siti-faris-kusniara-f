91003cb0fcdd2b803c3bc41f67d296f78dbad2bd
sitifariskusniaraf <145628937+sitifariskusniaraf@users.noreply.github.com>
2023-11-03 08:17:45 +0700

    Add files via upload

---
+++ PERTEMUAN01/TipeData.java
@@ -0,0 +1,22 @@
+package PERTEMUAN01;
+
+public class TipeData {
+    public static void main(String[] args) {
+      String nama = "Arra";
+        System.out.println("Nama : " + nama);
+      int umur = 20;
+        System.out.println("Umur : " + umur);
+      float berat = (float) 52.5;
+        System.out.println("Berat Badan : " + berat);
+      double panjang = 152.5;
+        System.out.println("Tinggi Badan : " + panjang);
+      char gender = 'P';
+        System.out.println("Jenis Kelamin = " + gender);
+      boolean aktif = true;
+        System.out.println("Mahasiswa Aktif : " + aktif);
+      
+   
+     
+    }
+    
+}
